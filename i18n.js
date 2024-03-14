603d91cf45f5f81fc9bb737b8e19f24097506266
Lampros Karachristos <lamproskarachristos@yahoo.com>
2024-03-14 11:58:09 +0200

    New ui utilities are added

--- i18n.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    locales: ['en', 'gr'], // Array with the languages that you want to use
-    defaultLocale: 'gr', // Default language of your website
-    pages: {
-      '*': ['common'],
-    },
-};
