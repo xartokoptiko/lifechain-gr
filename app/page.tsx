db5366e4f20997659731e62551d3fc5e8f14aa69
Lampros Karachristos <lamproskarachristos@yahoo.com>
2024-05-25 21:41:19 +0300

    added better profuct section ui

--- app/page.tsx
+++ app/page.tsx
@@ -345,125 +345,259 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="flex flex-row w-full py-[100px] max-sm:flex-col">
-        <div className="flex flex-col w-1/2 items-center justify-center max-sm:w-full max-sm:mb-[100px]">
-          <img
-            src="/app/photos/homepage.png"
-            className="w-[172px] h-[372px] shadow-2xl shadow-red-800 rounded-xl 2xl:w-[210px] 2xl:h-[450px]"
-          />
-        </div>
-        <div className="flex flex-col w-1/2 items-center max-sm:w-full">
-          <div className="flex flex-col w-full lg:max-w-[300px] md:max-w-[300px] max-sm:px-[10px] max-sm:items-center">
-            <p className="text-3xl font-extrabold text-[#424242]">
-              Life Chain University
-            </p>
-            <p className="text-lg font-extrabold text-[#424242] mt-[15px]">
-              uni_app_catch
-            </p>
-            <div className="space-y-3 mt-[25px]">
-              <div className="flex flex-row items-center space-x-2">
-                <img src="/checklist.png" className="w-[25px] h-[25px]" />
-                <p className="text-[#424242] font-extrabold lg:font-bold md:font-bold">
-                  uni_app_one
-                </p>
+      <div className="flex flex-col w-full py-[100px] max-sm:flex-col items-center justify-center">
+        <section className="bg-white dark:bg-gray-900 max-w-4xl">
+          <div className="container px-6 py-10 mx-auto max-sm:flex-col">
+            <div className="lg:flex lg:items-center">
+              <div className="w-full space-y-12 lg:w-1/2 ">
+                <div>
+                  <h1 className="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">
+                    explore our <br /> awesome Components
+                  </h1>
+
+                  <div className="mt-2">
+                    <span className="inline-block w-40 h-1 bg-blue-500 rounded-full"></span>
+                    <span className="inline-block w-3 h-1 ml-1 bg-blue-500 rounded-full"></span>
+                    <span className="inline-block w-1 h-1 ml-1 bg-blue-500 rounded-full"></span>
+                  </div>
+                </div>
+
+                <div className="md:flex md:items-start md:-mx-4">
+                  <span className="inline-block p-2 text-blue-500 bg-blue-100 rounded-xl md:mx-4 dark:text-white dark:bg-blue-500">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
+                      />
+                    </svg>
+                  </span>
+
+                  <div className="mt-4 md:mx-4 md:mt-0">
+                    <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">
+                      Copy & paste components
+                    </h1>
+
+                    <p className="mt-3 text-gray-500 dark:text-gray-300">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Provident ab nulla quod dignissimos vel non corrupti
+                      doloribus voluptatum eveniet
+                    </p>
+                  </div>
+                </div>
+
+                <div className="md:flex md:items-start md:-mx-4">
+                  <span className="inline-block p-2 text-blue-500 bg-blue-100 rounded-xl md:mx-4 dark:text-white dark:bg-blue-500">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+                      />
+                    </svg>
+                  </span>
+
+                  <div className="mt-4 md:mx-4 md:mt-0">
+                    <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">
+                      Zero Configuration
+                    </h1>
+
+                    <p className="mt-3 text-gray-500 dark:text-gray-300">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Provident ab nulla quod dignissimos vel non corrupti
+                      doloribus voluptatum eveniet
+                    </p>
+                  </div>
+                </div>
+
+                <div className="md:flex md:items-start md:-mx-4">
+                  <span className="inline-block p-2 text-blue-500 bg-blue-100 rounded-xl md:mx-4 dark:text-white dark:bg-blue-500">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z"
+                      />
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z"
+                      />
+                    </svg>
+                  </span>
+
+                  <div className="mt-4 md:mx-4 md:mt-0">
+                    <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">
+                      elegant Dark Mode
+                    </h1>
+
+                    <p className="mt-3 text-gray-500 dark:text-gray-300">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Provident ab nulla quod dignissimos vel non corrupti
+                      doloribus voluptatum eveniet
+                    </p>
+                  </div>
+                </div>
               </div>
 
-              <div className="flex flex-row items-center space-x-2">
-                <img src="/checklist.png" className="w-[25px] h-[25px]" />
-                <p className="text-[#424242] font-extrabold lg:font-bold md:font-bold">
-                  uni_app_two
-                </p>
-              </div>
-
-              <div className="flex flex-row items-center space-x-2">
-                <img src="/checklist.png" className="w-[25px] h-[25px]" />
-                <p className="text-[#424242] font-extrabold lg:font-bold md:font-bold">
-                  uni_app_three
-                </p>
-              </div>
-
-              <div className="flex flex-row items-center space-x-2">
-                <img src="/checklist.png" className="w-[25px] h-[25px]" />
-                <p className="text-[#424242] font-extrabold lg:font-bold md:font-bold">
-                  uni_app_four
-                </p>
-              </div>
-
-              <div className="flex flex-row items-center space-x-2">
-                <img src="/checklist.png" className="w-[25px] h-[25px]" />
-                <p className="text-[#424242] font-extrabold lg:font-bold md:font-bold">
-                  uni_app_five
-                </p>
+              <div className="flex flex-row flex-grow items-end justify-end">
+                <img
+                  className="w-[190px] h-[400px] rounded-lg shadow-xl shadow-redonebg"
+                  src="/app/photos/homeprivate.png"
+                  alt=""
+                />
               </div>
             </div>
-            <a
-              href="/university"
-              className="flex felx-col px-[20px] py-[10px] mt-[30px] items-center justify-center text-center rounded-xl bg-red-500 text-white cursor-pointer font-extrabold"
-            >
-              <p>learn</p>
-            </a>
           </div>
-        </div>
-      </div>
-      <div className="flex flex-row w-full py-[100px] max-sm:flex-col">
-        <div className="flex flex-col w-1/2 items-center justify-center max-sm:w-full max-sm:mb-[100px]">
-          <div className="flex flex-col w-full lg:max-w-[300px] md:max-w-[300px] max-sm:px-[10px] max-sm:items-center">
-            <p className="text-3xl font-extrabold text-[#424242] lg:font-bold md:font-bold">
-              Life Chain Travel
-            </p>
-            <p className="text-lg font-extrabold text-[#424242] mt-[15px] lg:font-bold md:font-bold">
-              trav_app_catch
-            </p>
-            <div className="space-y-3 mt-[25px]">
-              <div className="flex flex-row items-center space-x-2">
-                <img src="/checklist.png" className="w-[25px] h-[25px]" />
-                <p className="text-[#424242] font-extrabold lg:font-bold md:font-bold">
-                  rav_app_one
-                </p>
-              </div>
+        </section>
 
-              <div className="flex flex-row items-center space-x-2">
-                <img src="/checklist.png" className="w-[25px] h-[25px]" />
-                <p className="text-[#424242] font-extrabold lg:font-bold md:font-bold">
-                  trav_app_two
-                </p>
+        <section className="bg-white dark:bg-gray-900 mt-[100px]">
+          <div className="container px-6 py-10 mx-auto max-w-4xl">
+            <div className="lg:flex lg:items-center max-sm:flex-col">
+              <div className="flex flex-row flex-grow ">
+                <img
+                  className="w-[190px] h-[400px] rounded-lg shadow-xl shadow-redonebg"
+                  src="/app/photos/homepage.png"
+                  alt=""
+                />
               </div>
+              <div className="w-full space-y-12 lg:w-1/2 ">
+                <div>
+                  <h1 className="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">
+                    explore our <br /> awesome Components
+                  </h1>
 
-              <div className="flex flex-row items-center space-x-2">
-                <img src="/checklist.png" className="w-[25px] h-[25px]" />
-                <p className="text-[#424242] font-extrabold lg:font-bold md:font-bold">
-                  trav_app_three
-                </p>
-              </div>
+                  <div className="mt-2">
+                    <span className="inline-block w-40 h-1 bg-blue-500 rounded-full"></span>
+                    <span className="inline-block w-3 h-1 ml-1 bg-blue-500 rounded-full"></span>
+                    <span className="inline-block w-1 h-1 ml-1 bg-blue-500 rounded-full"></span>
+                  </div>
+                </div>
 
-              <div className="flex flex-row items-center space-x-2">
-                <img src="/checklist.png" className="w-[25px] h-[25px]" />
-                <p className="text-[#424242] font-extrabold lg:font-bold md:font-bold">
-                  trav_app_four
-                </p>
-              </div>
+                <div className="md:flex md:items-start md:-mx-4">
+                  <span className="inline-block p-2 text-blue-500 bg-blue-100 rounded-xl md:mx-4 dark:text-white dark:bg-blue-500">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
+                      />
+                    </svg>
+                  </span>
 
-              <div className="flex flex-row items-center space-x-2">
-                <img src="/checklist.png" className="w-[25px] h-[25px]" />
-                <p className="text-[#424242] font-extrabold lg:font-bold md:font-bold">
-                  trav_app_five
-                </p>
+                  <div className="mt-4 md:mx-4 md:mt-0">
+                    <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">
+                      Copy & paste components
+                    </h1>
+
+                    <p className="mt-3 text-gray-500 dark:text-gray-300">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Provident ab nulla quod dignissimos vel non corrupti
+                      doloribus voluptatum eveniet
+                    </p>
+                  </div>
+                </div>
+
+                <div className="md:flex md:items-start md:-mx-4">
+                  <span className="inline-block p-2 text-blue-500 bg-blue-100 rounded-xl md:mx-4 dark:text-white dark:bg-blue-500">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+                      />
+                    </svg>
+                  </span>
+
+                  <div className="mt-4 md:mx-4 md:mt-0">
+                    <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">
+                      Zero Configuration
+                    </h1>
+
+                    <p className="mt-3 text-gray-500 dark:text-gray-300">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Provident ab nulla quod dignissimos vel non corrupti
+                      doloribus voluptatum eveniet
+                    </p>
+                  </div>
+                </div>
+
+                <div className="md:flex md:items-start md:-mx-4">
+                  <span className="inline-block p-2 text-blue-500 bg-blue-100 rounded-xl md:mx-4 dark:text-white dark:bg-blue-500">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z"
+                      />
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z"
+                      />
+                    </svg>
+                  </span>
+
+                  <div className="mt-4 md:mx-4 md:mt-0">
+                    <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">
+                      elegant Dark Mode
+                    </h1>
+
+                    <p className="mt-3 text-gray-500 dark:text-gray-300">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Provident ab nulla quod dignissimos vel non corrupti
+                      doloribus voluptatum eveniet
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
-            <a
-              href="/travel"
-              className="flex felx-col px-[20px] py-[10px] mt-[30px] items-center justify-center text-center rounded-xl bg-red-500 text-white cursor-pointer font-extrabold"
-            >
-              <p>learn</p>
-            </a>
           </div>
-        </div>
-        <div className="flex flex-col items-center justify-center w-1/2 max-sm:w-full">
-          <img
-            src="/app/photos/homeprivate.png"
-            className="w-[172px] h-[372px] shadow-2xl shadow-red-800 rounded-xl 2xl:w-[210px] 2xl:h-[450px]"
-          />
-        </div>
+        </section>
       </div>
 
       <div className="py-[100px] grid grid-cols-3 max-sm:grid-cols-1 antialiased bg-red-500 dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
@@ -668,7 +802,9 @@ export default function Home() {
                   className="w-32 h-32 rounded-full inline-block"
                 />
                 <div className="py-4">
-                  <h4 className="text-base font-extrabold">Alkinnos Allissandrakis</h4>
+                  <h4 className="text-base font-extrabold">
+                    Alkinnos Allissandrakis
+                  </h4>
                   <p className="text-xs mt-1">Software Engineer</p>
                   <div className="space-x-4 mt-4">
                     <button
@@ -728,7 +864,9 @@ export default function Home() {
                   className="w-32 h-32 rounded-full inline-block"
                 />
                 <div className="py-4">
-                  <h4 className="text-base font-extrabold">Lampros Karachristos</h4>
+                  <h4 className="text-base font-extrabold">
+                    Lampros Karachristos
+                  </h4>
                   <p className="text-xs mt-1">Software Engineer</p>
                   <div className="space-x-4 mt-4">
                     <button
